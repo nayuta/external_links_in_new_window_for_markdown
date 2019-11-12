b106c74d5830135b55030bf520eb7e1f03216c4a
Nayuta Ishii <nayuta@gmail.com>
2019-11-12 15:18:02 +0900

    Fix: fix path

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 require 'redmine'
 
-require_dependency 'external_links_in_new_window/view_layouts_base_html_head_hook'
+require_dependency 'external_links_in_new_window_for_markdown/view_layouts_base_html_head_hook'
 
-Redmine::Plugin.register :external_links_in_new_window do
+Redmine::Plugin.register :external_links_in_new_window_for_markdown do
   name 'External Links In New Window'
   author 'akzhan.abdulin@gmail.com, lunakid@gmail.com'
   description 'Open external & attachment links in new browser windows.'
